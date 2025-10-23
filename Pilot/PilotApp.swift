2610458896f4154e99ee09f4ff70b36f114925bb
Tyler King <tylerking999@gmail.com>
2025-10-23 03:30:31 -0700

    Initial Commit

---
+++ Pilot/PilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  PilotApp.swift
+//  Pilot
+//
+//  Created by Tyler King on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct PilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
