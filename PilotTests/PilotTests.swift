ce9803903a2faeef1711d24f62cd8f66571aacb9
tyler king <tylerking999@gmail.com>
2025-10-23 22:45:12 -0700

    Initial Commit

---
+++ PilotTests/PilotTests.swift
@@ -0,0 +1,17 @@
+//
+//  PilotTests.swift
+//  PilotTests
+//
+//  Created by Tyler King on 10/23/25.
+//
+
+import Testing
+@testable import Pilot
+
+struct PilotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
